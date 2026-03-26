f7c8e02c377761f0bd0ee514db14a464bc4a54cc
SihleLucingo <110468067+SihleLucingo@users.noreply.github.com>
2026-03-26 17:05:01 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+# Client Website CMS WordPress Project
---
+++ theme/client-cms-theme/front-page.php
@@ -0,0 +1,4 @@
+<?php get_header(); ?>
+<h1><?php the_field('hero_title'); ?></h1>
+<p><?php the_field('hero_description'); ?></p>
+<?php get_footer(); ?>
